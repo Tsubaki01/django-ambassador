fd92bcdb440ca82b40aabf56af6c302945a97e20
Tsubaki01 <web.koharu01@gmail.com>
2022-07-11 23:36:58 +0000

    Userモデルの作成とマイグレーション設定

--- core/models.py
+++ core/models.py
@@ -4,3 +4,11 @@ from django.db import models
 # Create your models here.
 class User(AbstractUser):
     first_name = models.CharField(max_length=255)
+    lase_name = models.CharField(max_length=255)
+    email = models.CharField(max_length=255, unique=True)
+    password = models.CharField(max_length=255)
+    is_ambassador = models.BooleanField(default=True)
+    username = None
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
