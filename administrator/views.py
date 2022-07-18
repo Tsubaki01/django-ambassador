171a32d806f45d4bbcf97b77dac2babf49a77a01
Tsubaki01 <web.koharu01@gmail.com>
2022-07-18 17:44:20 +0900

    オーダーモデルの作成とオーダーデータ操作の実装

--- administrator/views.py
+++ administrator/views.py
@@ -3,10 +3,10 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .serializers import ProductSerializer, LinkSerializer#, OrderSerializer
+from .serializers import ProductSerializer, LinkSerializer, OrderSerializer
 from common.authentication import JWTAuthentication
 from common.serializers import UserSerializer
-from core.models import User, Product, Link#, Order
+from core.models import User, Product, Link, Order
 from django.core.cache import cache
 
 
@@ -77,4 +77,15 @@ class LinkAPIView(APIView):
     def get(self, request, pk=None):
         links = Link.objects.filter(user_id=pk)
         serializer = LinkSerializer(links, many=True)
+        return Response(serializer.data)
+
+
+# オーダー情報取得API
+class OrderAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        orders = Order.objects.filter(complete=True)
+        serializer = OrderSerializer(orders, many=True)
         return Response(serializer.data)
