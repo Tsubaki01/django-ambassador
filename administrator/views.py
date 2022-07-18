651403ca449e2daf87bc3666e98948b679d05382
Tsubaki01 <web.koharu01@gmail.com>
2022-07-18 16:23:21 +0900

    ambasaador情報の取得機能実装。Fakerをインストールしテストユーザーデータを30人分追加

--- administrator/views.py
+++ administrator/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from rest_framework import generics, mixins
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+# from .serializers import ProductSerializer, LinkSerializer, OrderSerializer
+from common.authentication import JWTAuthentication
+from common.serializers import UserSerializer
+from core.models import User#, Product, Link, Order
+# from django.core.cache import cache
+
+
+class AmbassadorAPIView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get(self, _):
+        ambassadors = User.objects.filter(is_ambassador=True)
+        serializer = UserSerializer(ambassadors, many=True)
+        return Response(serializer.data)
