17044a267b56f2045e8c50d565e2ca6b0af9afac
Tsubaki01 <web.koharu01@gmail.com>
2022-07-18 17:02:13 +0900

    製品モデルと製品の操作を実装

--- administrator/views.py
+++ administrator/views.py
@@ -3,13 +3,14 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-# from .serializers import ProductSerializer, LinkSerializer, OrderSerializer
+from .serializers import ProductSerializer#, LinkSerializer, OrderSerializer
 from common.authentication import JWTAuthentication
 from common.serializers import UserSerializer
-from core.models import User#, Product, Link, Order
-# from django.core.cache import cache
+from core.models import User, Product#, Link, Order
+from django.core.cache import cache
 
 
+# Ambassador情報取得機能API
 class AmbassadorAPIView(APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
@@ -18,3 +19,51 @@ class AmbassadorAPIView(APIView):
         ambassadors = User.objects.filter(is_ambassador=True)
         serializer = UserSerializer(ambassadors, many=True)
         return Response(serializer.data)
+
+
+# 製品汎用機能API
+class ProductGenericAPIView(
+    generics.GenericAPIView, mixins.RetrieveModelMixin, mixins.ListModelMixin, mixins.CreateModelMixin,
+    mixins.UpdateModelMixin, mixins.DestroyModelMixin
+):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    # 製品情報取得API
+    def get(self, request, pk=None):
+        if pk:
+            return self.retrieve(request, pk)
+
+        return self.list(request)
+
+    # 製品追加API
+    def post(self, request):
+        # response = self.create(request)
+        # for key in cache.keys('*'):
+        #     if 'products_frontend' in key:
+        #         cache.delete(key)
+        # cache.delete('products_backend')
+        # return response
+        return self.create(request)
+
+    # 製品更新API
+    def put(self, request, pk=None):
+        # response = self.partial_update(request, pk)
+        # for key in cache.keys('*'):
+        #     if 'products_frontend' in key:
+        #         cache.delete(key)
+        # cache.delete('products_backend')
+        # return response
+        return self.partial_update(request, pk)
+
+    # 製品削除API
+    def delete(self, request, pk=None):
+        # response = self.destroy(request, pk)
+        # for key in cache.keys('*'):
+        #     if 'products_frontend' in key:
+        #         cache.delete(key)
+        # cache.delete('products_backend')
+        # return response
+        return self.destroy(request, pk)
--- core/models.py
+++ core/models.py
@@ -2,7 +2,10 @@ from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
+
+# ユーザーマネージャー
 class UserManager(BaseUserManager):
+    # 一般ユーザーの作成
     def create_user(self, email, password=None):
         if not email:
             raise ValueError("User must have an email")
@@ -19,6 +22,7 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
+    # スーパーユーザー作成
     def create_superuser(self, email, password=None):
         if not email:
             raise ValueError("User must have an email")
@@ -36,6 +40,8 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
+
+# ユーザーモデル
 class User(AbstractUser):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
@@ -48,3 +54,13 @@ class User(AbstractUser):
     REQUIRED_FIELDS = []
 
     objects = UserManager()
+
+
+# 製品モデル
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=1000, null=True)
+    image = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+
