1176cfd89a4d82b5cf15e5c1d6fd9975623274da
Tsubaki01 <web.koharu01@gmail.com>
2022-07-18 13:59:21 +0900

    ユーザー登録APIの実装

---
+++ common/views.py
@@ -0,0 +1,53 @@
+from rest_framework import exceptions
+# from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from core.models import User
+from .authentication import JWTAuthentication
+from .serializers import UserSerializer
+
+
+class RegisterAPIView(APIView):
+    def post(self, request):
+        data = request.data
+
+        if data['password'] != data['password_confirm']:
+            raise exceptions.APIException('Passwords do not match!')
+
+        # data['is_ambassador'] = 'api/ambassador' in request.path
+        data['is_ambassador'] = 0
+
+        serializer = UserSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
+
+
+class LoginAPIView(APIView):
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            raise exceptions.AuthenticationFailed('User not found!')
+
+        if not user.check_password(password):
+            raise exceptions.AuthenticationFailed('Incorrect Password!')
+
+        scope = 'ambassador' if 'api/ambassador' in request.path else 'admin'
+
+        if user.is_ambassador and scope == 'admin':
+            raise exceptions.AuthenticationFailed('Unauthorized')
+
+        token = JWTAuthentication.generate_jwt(user.id, scope)
+
+        response = Response()
+        response.set_cookie(key='jwt', value=token, httponly=True)
+        response.data = {
+            'message': 'success'
+        }
+
+        return response
